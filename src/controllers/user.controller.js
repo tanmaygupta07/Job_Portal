d03024fcdd866068b8d8b4dd1d4f86125839eb64
Tanmay Gupta <gupta.tanmay.tg@gmail.com>
2024-09-07 01:10:52 +0530

    User Controller Completed

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,45 @@
+import UserModel from "../models/user.model.js";
+
+export default class UserController {
+    //renser user login
+    getLogin = (req, res, next) => {
+        res.render("user-login", { errors: null });
+    }
+
+    //add new user
+    addUser = (req, res) => {
+        UserModel.addUser(req.body);
+        res.redirect('/login');
+    }
+
+    //user login
+    userLogin = (req, res) => {
+        const { email, password } = req.body;
+        const userToValidate = UserModel.validateLogin(req.body);
+        if (!userToValidate) {
+            res.render("404", { msg: "User not found. Please register to continue." })
+        }
+
+        if (userToValidate.email === email && userToValidate.password === password) {
+            req.session.user = userToValidate;
+            res.redirect("/jobs");
+        }
+        else {
+            res.render("404", { msg: "Invalid credentials" })
+        }
+    }
+
+    //logout user
+    userLogout = (req, res) => {
+        req.session.userEmail = null;
+        res.clearCookie("lastVisit");
+        req.session.destroy((err) => {
+            if (err) {
+                console.log(err);
+            }
+            else {
+                res.redirect('/login');
+            }
+        })
+    }
+}
